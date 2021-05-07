6b4b8381a40e3864dae0507205e81bade8137ada
zoraaver <zoraaver@gmail.com>
2021-05-07 18:43:34 +0100

    Main app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,80 +1,24 @@
-import React from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
-
-import { Colors, Header } from "react-native/Libraries/NewAppScreen";
-
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  useDispatch,
-  TypedUseSelectorHook,
-  useSelector,
-  Provider,
-} from "react-redux";
-
-import { Section } from "./Section";
-import { userReducer } from "../slices/usersSlice";
-import { workoutLogsReducer } from "../slices/workoutLogsSlice";
-import { workoutPlansReducer } from "../slices/workoutPlansSlice";
-import { workoutsReducer } from "../slices/workoutsSlice";
-
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    workoutPlans: workoutPlansReducer,
-    workoutLogs: workoutLogsReducer,
-    workouts: workoutsReducer,
-  },
-});
-
-type RootState = ReturnType<typeof store.getState>;
-type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+import * as React from "react";
+import { Text } from "react-native";
+import { useAppDispatch, useAppSelector } from "..";
+import LoginScreen from "../screens/LoginScreen";
+import { validateUser } from "../slices/usersSlice";
 
 export function App() {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <Provider store={store}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}
-        >
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}
-          >
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.js</Text> to change this
-              screen and then come back to see your editing.
-            </Section>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Provider>
+  const dispatch = useAppDispatch();
+  const authenticationStatus = useAppSelector(
+    (state) => state.user.authenticationStatus
   );
+
+  React.useEffect(() => {
+    dispatch(validateUser());
+  }, []);
+
+  switch (authenticationStatus) {
+    case "unknown":
+    case "pending":
+      return <LoginScreen />;
+    case "confirmed":
+      return <Text>Hello!</Text>;
+  }
 }
-
-const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: "700",
-  },
-});
-
-export default App;
