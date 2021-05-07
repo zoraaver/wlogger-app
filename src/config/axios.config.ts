c9aa70d6423b09de6be55730a80e4411117a723f
zoraaver <zoraaver@gmail.com>
2021-05-07 18:43:22 +0100

    Change backend dev url to match ios/android emulator settings

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { Platform } from "react-native";
 
 const backendUrl: string = "https://wlogger.uk/api";
-const developmentUrl: string = "http://localhost:8080";
+const developmentUrl: string =
+  Platform.OS === "ios" ? "http://localhost:8080" : "http://10.0.2.2:8080";
 
 export const baseURL =
   process.env.NODE_ENV === "production" ? backendUrl : developmentUrl;
