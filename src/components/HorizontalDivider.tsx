0fbbafdb0891236805673bee3d76466825da2a00
zoraaver <zoraaver@gmail.com>
2021-05-08 16:12:48 +0100

    Divider between two login buttons

---
+++ src/components/HorizontalDivider.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+interface HorizontalDividerProps {
+  height?: number;
+  text: string;
+  backgroundColor: string;
+}
+export function HorizontalDivider({
+  height = 1,
+  text,
+  backgroundColor,
+}: HorizontalDividerProps) {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View style={{ flex: 1, height, backgroundColor }} />
+      <View>
+        <Text style={{ width: 40, textAlign: "center", fontSize: 18 }}>
+          {text}
+        </Text>
+      </View>
+      <View style={{ flex: 1, height, backgroundColor }} />
+    </View>
+  );
+}
