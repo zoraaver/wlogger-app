d9807acdc45b58ca8fb5600a8d0d7b8caaa88977
zoraaver <zoraaver@gmail.com>
2021-05-08 16:12:24 +0100

    Configure google sign in before main app loads

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { configureStore } from "@reduxjs/toolkit";
 import * as React from "react";
 import {
@@ -25,6 +26,17 @@ type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+const googleiOSClientId: string =
+  "695443910196-nvpotp45hi701vja07bomtpmqeo7395p.apps.googleusercontent.com";
+const googleWebClientId: string =
+  "695443910196-mc7763ul6h5k5kgf1p08hjfn2pv7kccs.apps.googleusercontent.com";
+
+GoogleSignin.configure({
+  iosClientId: googleiOSClientId,
+  webClientId: googleWebClientId,
+  scopes: ["email"],
+});
 
 export function Application() {
   return (
