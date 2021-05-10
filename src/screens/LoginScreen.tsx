6b6032d2855931750178ad24ff7a1f20b04895d7
zoraaver <zoraaver@gmail.com>
2021-05-10 19:35:08 +0100

    Add signup modal and divider between login form and sign up button

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,27 +1,37 @@
 import * as React from "react";
 import { View, StyleSheet, SafeAreaView, Text } from "react-native";
-import { useAppSelector } from "..";
+import { Button } from "../components/Button";
+import { HorizontalDivider } from "../components/HorizontalDivider";
 import LoginForm from "../components/LoginForm";
-import { BalsamiqSans, infoColor } from "../util/constants";
+import { SignupModal } from "../components/SignupModal";
+import {
+  BalsamiqSans,
+  Helvetica,
+  infoColor,
+  successColor,
+} from "../util/constants";
 
 export default function LoginScreen() {
-  const loginError = useAppSelector((state) => state.user.loginError);
+  const [showSignupModal, setShowSignupModal] = React.useState(false);
 
   return (
     <>
       <SafeAreaView style={styles.topSafeAreaView} />
       <SafeAreaView style={styles.loginScreen}>
+        <SignupModal
+          showSignupModal={showSignupModal}
+          setShowSignupModal={setShowSignupModal}
+        />
         <View style={styles.logoArea}>
           <Text style={styles.logoHeader}>wLogger</Text>
           <Text style={styles.logoText}>Track and log workouts</Text>
         </View>
         <View style={styles.inputArea}>
-          {loginError && (
-            <View>
-              <Text style={{ color: "red" }}>{loginError}</Text>
-            </View>
-          )}
           <LoginForm />
+          <HorizontalDivider backgroundColor="powderblue" text="or" />
+          <Button color={successColor} onPress={() => setShowSignupModal(true)}>
+            <Text style={styles.signupButtonText}>Create an account</Text>
+          </Button>
         </View>
       </SafeAreaView>
     </>
@@ -45,11 +55,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: infoColor,
     padding: 20,
+    justifyContent: "space-between",
   },
   loginScreen: {
     flex: 1,
     justifyContent: "space-around",
     alignItems: "stretch",
     backgroundColor: infoColor,
+  },
+  signupButtonText: {
+    fontFamily: Helvetica,
+    color: "white",
+    fontSize: 18,
   },
 });
