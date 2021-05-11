b7771c5f9f4efdcf9429aae68a3f69d8a90932dd
zoraaver <zoraaver@gmail.com>
2021-05-11 18:34:37 +0100

    Add animation to shrink top half of screen when the keyboard comes up

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,17 @@
 import * as React from "react";
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Animated,
+  Platform,
+  Keyboard,
+  KeyboardEventName,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import { Button } from "../components/Button";
+import { DismissKeyboard } from "../components/DismissKeyboard";
 import { HorizontalDivider } from "../components/HorizontalDivider";
 import LoginForm from "../components/LoginForm";
 import { SignupModal } from "../components/SignupModal";
@@ -13,27 +24,72 @@ import {
 
 export default function LoginScreen() {
   const [showSignupModal, setShowSignupModal] = React.useState(false);
+  const windowHeight = useWindowDimensions().height;
+  const logoAreaMaxHeight = React.useRef(new Animated.Value(windowHeight))
+    .current;
+
+  React.useEffect(() => {
+    const keyboardEvents = Platform.select({
+      ios: ["keyboardWillShow", "keyboardWillHide"],
+      android: ["keyboardDidShow", "keyboardDidHide"],
+    }) as KeyboardEventName[];
+    Keyboard.addListener(keyboardEvents[0], decreaseLogoAreaHeightAnimation);
+    Keyboard.addListener(keyboardEvents[1], increaseLogoAreaHeightAniimation);
+    return () => {
+      Keyboard.removeListener(
+        keyboardEvents[0],
+        decreaseLogoAreaHeightAnimation
+      );
+      Keyboard.removeListener(
+        keyboardEvents[1],
+        increaseLogoAreaHeightAniimation
+      );
+    };
+  }, []);
+
+  function decreaseLogoAreaHeightAnimation(event: any) {
+    Animated.timing(logoAreaMaxHeight, {
+      toValue: windowHeight / 4.5,
+      duration: event.duration * 1.5,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function increaseLogoAreaHeightAniimation(event: any) {
+    Animated.timing(logoAreaMaxHeight, {
+      toValue: windowHeight,
+      duration: event.duration * 1.5,
+      useNativeDriver: false,
+    }).start();
+  }
 
   return (
     <>
       <SafeAreaView style={styles.topSafeAreaView} />
-      <SafeAreaView style={styles.loginScreen}>
-        <SignupModal
-          showSignupModal={showSignupModal}
-          setShowSignupModal={setShowSignupModal}
-        />
-        <View style={styles.logoArea}>
-          <Text style={styles.logoHeader}>wLogger</Text>
-          <Text style={styles.logoText}>Track and log workouts</Text>
-        </View>
-        <View style={styles.inputArea}>
-          <LoginForm />
-          <HorizontalDivider backgroundColor="powderblue" text="or" />
-          <Button color={successColor} onPress={() => setShowSignupModal(true)}>
-            <Text style={styles.signupButtonText}>Create an account</Text>
-          </Button>
-        </View>
-      </SafeAreaView>
+      <DismissKeyboard>
+        <SafeAreaView style={styles.loginScreen}>
+          <SignupModal
+            showSignupModal={showSignupModal}
+            setShowSignupModal={setShowSignupModal}
+          />
+          <Animated.View
+            style={[styles.logoArea, { maxHeight: logoAreaMaxHeight }]}
+          >
+            <Text style={styles.logoHeader}>wLogger</Text>
+            <Text style={styles.logoText}>Track and log workouts</Text>
+          </Animated.View>
+          <View style={styles.inputArea}>
+            <LoginForm />
+            <HorizontalDivider backgroundColor="powderblue" text="or" />
+            <Button
+              color={successColor}
+              onPress={() => setShowSignupModal(true)}
+            >
+              <Text style={styles.signupButtonText}>Create an account</Text>
+            </Button>
+          </View>
+        </SafeAreaView>
+      </DismissKeyboard>
     </>
   );
 }
