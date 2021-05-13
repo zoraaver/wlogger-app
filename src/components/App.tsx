bbde4afe19c9767b212b38069b1d8fa8f7d843d4
zoraaver <zoraaver@gmail.com>
2021-05-13 19:25:49 +0100

    Icons for tab navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
 import * as React from "react";
 import { useAppDispatch, useAppSelector } from "..";
 import { HomeScreen } from "../screens/HomeScreen";
@@ -10,14 +13,45 @@ import { validateUser } from "../slices/usersSlice";
 import Ionicon from "react-native-vector-icons/Ionicons";
 import { primaryColor } from "../util/constants";
 import { WorkoutPlansScreen } from "../screens/WorkoutPlansScreen";
+import { RouteProp } from "@react-navigation/native";
+
+function tabScreenOptions({
+  route,
+}: {
+  route: RouteProp<Record<string, object | undefined>, string>;
+  navigation: any;
+}): BottomTabNavigationOptions {
+  return {
+    tabBarIcon: ({ color, size }) => {
+      let iconName: string = "";
+      switch (route.name) {
+        case "Home":
+          iconName = "home";
+          break;
+        case "Plans":
+          iconName = "clipboard";
+          break;
+        case "Settings":
+          iconName = "settings";
+          break;
+        case "Logs":
+          iconName = "journal";
+          break;
+        default:
+          break;
+      }
+      return <Ionicon name={iconName} size={size} color={color} />;
+    },
+  };
+}
+
+const Tab = createBottomTabNavigator();
 
 export function App() {
   const dispatch = useAppDispatch();
   const authenticationStatus = useAppSelector(
     (state) => state.user.authenticationStatus
   );
-
-  const Tab = createBottomTabNavigator();
 
   React.useEffect(() => {
     dispatch(validateUser());
@@ -36,28 +70,7 @@ export function App() {
             inactiveTintColor: "grey",
             keyboardHidesTabBar: true,
           }}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName: string = "";
-              switch (route.name) {
-                case "Home":
-                  iconName = "home";
-                  break;
-                case "Plans":
-                  iconName = "clipboard";
-                  break;
-                case "Settings":
-                  iconName = "settings";
-                  break;
-                case "Logs":
-                  iconName = "journal";
-                  break;
-                default:
-                  break;
-              }
-              return <Ionicon name={iconName} size={size} color={color} />;
-            },
-          })}
+          screenOptions={tabScreenOptions}
         >
           <Tab.Screen component={WorkoutLogsScreen} name="Logs" />
           <Tab.Screen component={WorkoutPlansScreen} name="Plans" />
