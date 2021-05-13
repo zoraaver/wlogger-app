e244abbb7ce75e01983c369bde3a009077833179
zoraaver <zoraaver@gmail.com>
2021-05-13 18:15:34 +0100

    Create tab navigator in main app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,11 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
-import { Text } from "react-native";
 import { useAppDispatch, useAppSelector } from "..";
+import { HomeScreen } from "../screens/HomeScreen";
+import { LoadingScreen } from "../screens/LoadingScreen";
 import LoginScreen from "../screens/LoginScreen";
+import { SettingsScreen } from "../screens/SettingsScreen";
+import { WorkoutLogsScreen } from "../screens/WorkoutLogsScreen";
 import { validateUser } from "../slices/usersSlice";
 
 export function App() {
@@ -10,15 +14,24 @@ export function App() {
     (state) => state.user.authenticationStatus
   );
 
+  const Tab = createBottomTabNavigator();
+
   React.useEffect(() => {
     dispatch(validateUser());
   }, []);
 
   switch (authenticationStatus) {
     case "unknown":
-    case "pending":
       return <LoginScreen />;
+    case "pending":
+      return <LoadingScreen />;
     case "confirmed":
-      return <Text>Hello!</Text>;
+      return (
+        <Tab.Navigator>
+          <Tab.Screen component={WorkoutLogsScreen} name="Logs" />
+          <Tab.Screen component={HomeScreen} name="Home" />
+          <Tab.Screen component={SettingsScreen} name="Settings" />
+        </Tab.Navigator>
+      );
   }
 }
