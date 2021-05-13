0b62875ae4d3c5bda8e4cfaff4b2918cdd0fec08
zoraaver <zoraaver@gmail.com>
2021-05-13 18:59:26 +0100

    Workout Plans screen

---
+++ src/screens/WorkoutPlansScreen.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export function WorkoutPlansScreen() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
