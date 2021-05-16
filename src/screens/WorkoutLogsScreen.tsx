3dfddddafb53fe372c9432db746fe5364f6e486e
zoraaver <zoraaver@gmail.com>
2021-05-16 23:15:40 +0100

    Move header title to workout log navigator

--- src/screens/WorkoutLogsScreen.tsx
+++ src/screens/WorkoutLogsScreen.tsx
@@ -1,6 +1,5 @@
-import { useFocusEffect } from "@react-navigation/core";
+import { useFocusEffect, useNavigation } from "@react-navigation/core";
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppDispatch, useAppSelector } from "..";
@@ -9,10 +8,10 @@ import {
   getWorkoutLogs,
   workoutLogHeaderData,
 } from "../slices/workoutLogsSlice";
-import { BalsamiqSans, infoColor } from "../util/constants";
 
 export function WorkoutLogsScreen() {
   const dispatch = useAppDispatch();
+  const navigation = useNavigation();
 
   useFocusEffect(
     React.useCallback(() => {
@@ -25,30 +24,16 @@ export function WorkoutLogsScreen() {
   );
 
   return (
-    <>
-      <SafeAreaView style={styles.topSafeAreaView} edges={["top"]} />
-      <SafeAreaView
-        style={styles.workoutLogsScreen}
-        edges={["bottom", "right", "left"]}
-      >
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Logs</Text>
-        </View>
-        <FlatList
-          style={styles.workoutLogsList}
-          data={workoutLogs}
-          keyExtractor={(workoutLog: workoutLogHeaderData) => workoutLog._id}
-          renderItem={WorkoutLogItem}
-        />
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={{ flex: 1 }} edges={["bottom", "right", "left"]}>
+      <FlatList
+        style={{ flex: 1 }}
+        data={workoutLogs.map((workoutLog) => ({
+          ...workoutLog,
+          navigation,
+        }))}
+        keyExtractor={(workoutLog: workoutLogHeaderData) => workoutLog._id}
+        renderItem={WorkoutLogItem}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  topSafeAreaView: { backgroundColor: infoColor },
-  workoutLogsScreen: { flex: 1 },
-  header: { alignItems: "center", padding: 10, backgroundColor: infoColor },
-  headerText: { fontSize: 25, fontFamily: BalsamiqSans },
-  workoutLogsList: { flex: 1 },
-});
