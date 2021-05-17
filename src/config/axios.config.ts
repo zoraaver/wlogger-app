f545054fd9206d92c019cdc1f54ec54bec811fc6
zoraaver <zoraaver@gmail.com>
2021-05-17 14:35:19 +0100

    Switch ios dev url to use mbp ip address so iphone device can access backend server

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -4,7 +4,7 @@ import EncryptedStorage from "react-native-encrypted-storage";
 
 const backendUrl: string = "https://wlogger.uk/api";
 const developmentUrl: string =
-  Platform.OS === "ios" ? "http://localhost:8080" : "http://10.0.2.2:8080";
+  Platform.OS === "ios" ? "http://192.168.86.21:8080" : "http://10.0.2.2:8080";
 
 export const baseURL =
   process.env.NODE_ENV === "production" ? backendUrl : developmentUrl;
