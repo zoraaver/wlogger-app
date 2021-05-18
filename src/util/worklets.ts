497b51ef24b265723ccae08959bce6e60a3eda28
zoraaver <zoraaver@gmail.com>
2021-05-18 13:14:02 +0100

    Add helper worklet to find nearest snap point

---
+++ src/util/worklets.ts
@@ -0,0 +1,15 @@
+export function findNearestSnapPoint(
+  position: number,
+  velocity: number,
+  snapPoints: number[]
+) {
+  "worklet";
+  const currentPosition: number = position + velocity * 0.2;
+  const positionDeltas: number[] = snapPoints.map((snapPoint) =>
+    Math.abs(snapPoint - currentPosition)
+  );
+  const minPositionDelta: number = Math.min(...positionDeltas);
+  return snapPoints.find(
+    (snapPoint) => Math.abs(snapPoint - currentPosition) === minPositionDelta
+  ) as number;
+}
