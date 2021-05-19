8aa275bf9c2ea958a972597f92272bb285c101e5
zoraaver <zoraaver@gmail.com>
2021-05-19 19:28:39 +0100

    Add log videos

--- src/screens/WorkoutLogScreen.tsx
+++ src/screens/WorkoutLogScreen.tsx
@@ -4,11 +4,17 @@ import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useAppDispatch, useAppSelector } from "..";
 import { WorkoutLogStackParamList } from "../navigators/WorkoutLogStackNavigator";
-import { getWorkoutLog, workoutLogData } from "../slices/workoutLogsSlice";
+import {
+  getWorkoutLog,
+  videoSetsSelector,
+  workoutLogData,
+} from "../slices/workoutLogsSlice";
 import { LoadingScreen } from "./LoadingScreen";
 import { Helvetica } from "../util/constants";
 import { ScrollView } from "react-native-gesture-handler";
 import { WorkoutLogTable } from "../containers/WorkoutLogTable";
+import EncryptedStorage from "react-native-encrypted-storage";
+import { WorkoutLogVideo } from "../components/WorkoutLogVideo";
 
 type WorkoutLogScreenRouteProp = RouteProp<WorkoutLogStackParamList, "show">;
 
@@ -17,11 +23,18 @@ export function WorkoutLogScreen() {
   const workoutLog: workoutLogData = useAppSelector(
     (state) => state.workoutLogs.editWorkoutLog
   );
+  const setsWithVideos = useAppSelector(videoSetsSelector);
+  const [token, setToken] = React.useState("");
   const dispatch = useAppDispatch();
 
   useFocusEffect(
     React.useCallback(() => {
       dispatch(getWorkoutLog(id));
+      EncryptedStorage.getItem("token").then((result) => {
+        if (result) {
+          setToken(result);
+        }
+      });
     }, [id])
   );
 
@@ -31,6 +44,18 @@ export function WorkoutLogScreen() {
   return (
     <ScrollView style={styles.workoutLogScreen}>
       <WorkoutLogTable workoutLog={workoutLog} />
+      {setsWithVideos.length ? (
+        <View style={styles.videoArea}>
+          <View style={styles.videoHeader}>
+            <Text style={styles.videoHeaderText}>Form videos</Text>
+          </View>
+          {token
+            ? setsWithVideos.map((set) => (
+                <WorkoutLogVideo set={set} authToken={token} key={set._id} />
+              ))
+            : null}
+        </View>
+      ) : null}
       {workoutLog.notes && (
         <View>
           <Text style={styles.workoutLogNotesText}>
@@ -59,5 +84,19 @@ const styles = StyleSheet.create({
     fontFamily: Helvetica,
     fontSize: 18,
     padding: 20,
+  },
+  videoHeader: {
+    backgroundColor: "aliceblue",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  videoHeaderText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: Helvetica,
+  },
+  videoArea: {
+    marginVertical: 20,
+    borderTopWidth: 0.2,
   },
 });
