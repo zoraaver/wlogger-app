9f376b694af4a7da0b7fd7d86ae34cd91480ad24
zoraaver <zoraaver@gmail.com>
2021-05-19 13:45:42 +0100

    Remove border props

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -4,25 +4,10 @@ import { View, StyleSheet, ViewStyle } from "react-native";
 interface RowProps {
   children: JSX.Element[] | JSX.Element;
   style?: ViewStyle;
-  borderWidth?: number;
-  topBorder?: boolean;
 }
 
-export function Row({ children, style, borderWidth, topBorder }: RowProps) {
-  return (
-    <View
-      style={[
-        styles.row,
-        { borderBottomWidth: borderWidth, borderLeftWidth: borderWidth },
-        topBorder && {
-          borderTopWidth: borderWidth,
-        },
-        style,
-      ]}
-    >
-      {children}
-    </View>
-  );
+export function Row({ children, style }: RowProps) {
+  return <View style={[styles.row, style]}>{children}</View>;
 }
 
 const styles = StyleSheet.create({
