f3dc6296341e6886d7b229f7cddb1676a59cd0d0
zoraaver <zoraaver@gmail.com>
2021-05-20 19:53:21 +0100

    Convenience  export of ionicon wrapped in animated component

---
+++ src/components/AnimatedIonicon.tsx
@@ -0,0 +1,4 @@
+import Animated from "react-native-reanimated";
+import Ionicon from "react-native-vector-icons/Ionicons";
+
+export const AnimatedIonicon = Animated.createAnimatedComponent(Ionicon);
