517740534abbe1b534b9fd4fb9b66fc09de07c6e
zoraaver <zoraaver@gmail.com>
2021-05-21 20:05:25 +0100

    Clear cache directory of any video files on app startup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,7 @@ import { LoadingScreen } from "../screens/LoadingScreen";
 import LoginScreen from "../screens/LoginScreen";
 import { validateUser } from "../slices/usersSlice";
 import { HomeTabNavigator } from "../navigators/HomeTabNavigator";
+import { cleanCacheDirectory } from "../slices/workoutLogsSlice";
 
 export function App() {
   const dispatch = useAppDispatch();
@@ -13,6 +14,7 @@ export function App() {
 
   React.useEffect(() => {
     dispatch(validateUser());
+    dispatch(cleanCacheDirectory());
   }, []);
 
   switch (authenticationStatus) {
