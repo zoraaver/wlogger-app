7b8874dcfe000f1054dcc39422f3cefe43b61aae
zoraaver <zoraaver@gmail.com>
2021-05-26 19:35:42 +0100

    Add video screen and upload screen

--- src/navigators/NewWorkoutLogStackNavigator.tsx
+++ src/navigators/NewWorkoutLogStackNavigator.tsx
@@ -3,12 +3,21 @@ import {
   StackNavigationProp,
 } from "@react-navigation/stack";
 import * as React from "react";
+import { RecordResponse } from "react-native-camera";
 import { NewWorkoutLogScreen } from "../screens/NewWorkoutLogScreen";
 import { WorkoutLogCameraScreen } from "../screens/WorkoutLogCameraScreen";
+import { WorkoutLogVideoScreen } from "../screens/WorkoutLogVideoScreen";
+import { WorkoutLogVideoUploadScreen } from "../screens/WorkoutLogVideoUploadScreen";
+import {
+  workoutLogScreenStyles,
+  WorkoutLogStackParamList,
+} from "./WorkoutLogStackNavigator";
 
 export type NewWorkoutLogStackParamList = {
-  logForm: undefined;
+  logForm: RecordResponse & { cancelled: boolean };
   camera: undefined;
+  showVideo: WorkoutLogStackParamList["showVideo"];
+  upload: undefined;
 };
 
 export type NewLogNavigation = StackNavigationProp<NewWorkoutLogStackParamList>;
@@ -21,8 +30,25 @@ export function NewWorkoutLogStackNavigator() {
       initialRouteName="logForm"
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="logForm" component={NewWorkoutLogScreen} />
+      <Stack.Screen
+        name="logForm"
+        component={NewWorkoutLogScreen}
+        initialParams={{ cancelled: true }}
+      />
       <Stack.Screen name="camera" component={WorkoutLogCameraScreen} />
+      <Stack.Screen
+        name="showVideo"
+        component={WorkoutLogVideoScreen}
+        options={({ route }) => ({
+          title: route.params.videoTitle,
+          headerShown: true,
+          headerStyle: workoutLogScreenStyles.screenHeader,
+          headerTitleStyle: workoutLogScreenStyles.screenHeaderTitle,
+          headerTitleAlign: "center",
+          headerBackTitle: "New log",
+        })}
+      />
+      <Stack.Screen name="upload" component={WorkoutLogVideoUploadScreen} />
     </Stack.Navigator>
   );
 }
