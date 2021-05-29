fffee3482a67d05cde4e4b5715aa3fa383e09eff
zoraaver <zoraaver@gmail.com>
2021-05-29 13:42:08 +0100

    Display flat list of workout plans

--- src/screens/WorkoutPlansScreen.tsx
+++ src/screens/WorkoutPlansScreen.tsx
@@ -1,10 +1,39 @@
+import { useFocusEffect } from "@react-navigation/core";
 import * as React from "react";
-import { View, Text } from "react-native";
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAppDispatch, useAppSelector } from "..";
+import { HorizontalDivider } from "../components/HorizontalDivider";
+import { WorkoutPlanItem } from "../components/WorkoutPlanItem";
+import {
+  getWorkoutPlans,
+  workoutPlanHeaderData,
+} from "../slices/workoutPlansSlice";
 
 export function WorkoutPlansScreen() {
+  const dispatch = useAppDispatch();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      dispatch(getWorkoutPlans());
+    }, [])
+  );
+
+  const workoutPlans: workoutPlanHeaderData[] = useAppSelector(
+    (state) => state.workoutPlans.data
+  );
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView edges={["bottom", "left", "right"]} style={{ flex: 1 }}>
+      <FlatList
+        style={{ flex: 1 }}
+        data={workoutPlans}
+        keyExtractor={(workoutPlan) => workoutPlan._id}
+        renderItem={(data) => <WorkoutPlanItem workoutPlan={data.item} />}
+        ItemSeparatorComponent={() => (
+          <HorizontalDivider backgroundColor="lightgrey" />
+        )}
+      />
+    </SafeAreaView>
   );
 }
