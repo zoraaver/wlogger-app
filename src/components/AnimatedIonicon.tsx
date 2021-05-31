528882d6b7e5dde851cf5936147c25b343dcab11
zoraaver <zoraaver@gmail.com>
2021-05-31 16:18:36 +0100

    Move AnimatedIonicon into animated swipe button file

--- src/components/AnimatedIonicon.tsx
+++
@@ -1,4 +0,0 @@
-import Animated from "react-native-reanimated";
-import Ionicon from "react-native-vector-icons/Ionicons";
-
-export const AnimatedIonicon = Animated.createAnimatedComponent(Ionicon);
