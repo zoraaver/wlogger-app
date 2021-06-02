fb8e7b1f1750eff9a38c5cd3cfd522c1697626e7
zoraaver <zoraaver@gmail.com>
2021-06-02 16:36:01 +0100

    Extract left/right button styles to separate variables

--- src/components/WeightUnitButtons.tsx
+++ src/components/WeightUnitButtons.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ViewStyle } from "react-native";
 import { weightUnit } from "../slices/workoutPlansSlice";
 import { Helvetica } from "../util/constants";
 import { Button } from "./Button";
@@ -7,35 +7,33 @@ import { Button } from "./Button";
 interface WeightUnitButtonsProps {
   unit: weightUnit;
   onUnitChange: (unit: weightUnit) => void;
+  height?: number;
 }
 
 export function WeightUnitButtons({
   unit,
   onUnitChange,
+  height = 40,
 }: WeightUnitButtonsProps) {
+  const kgBorderStyle: ViewStyle = {
+    borderWidth: unit === "kg" ? 2 : 0,
+    height,
+  };
+  const lbBorderStyle: ViewStyle = {
+    borderWidth: unit === "lb" ? 2 : 0,
+    height,
+  };
   return (
     <>
       <Button
         onPress={() => onUnitChange("kg")}
-        style={[
-          styles.unitButton,
-          styles.leftUnitButton,
-          {
-            borderWidth: unit === "kg" ? 2 : 0,
-          },
-        ]}
+        style={[styles.unitButton, styles.leftUnitButton, kgBorderStyle]}
       >
         <Text style={styles.buttonText}>kg</Text>
       </Button>
       <Button
         onPress={() => onUnitChange("lb")}
-        style={[
-          styles.unitButton,
-          styles.rightUnitButton,
-          {
-            borderWidth: unit === "lb" ? 2 : 0,
-          },
-        ]}
+        style={[styles.unitButton, styles.rightUnitButton, lbBorderStyle]}
       >
         <Text style={styles.buttonText}>lb</Text>
       </Button>
