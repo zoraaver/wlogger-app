81d714090ef6abce0d89c17d853d4df0f1d39499
zoraaver <zoraaver@gmail.com>
2021-06-02 16:39:21 +0100

    Add new workout modal data to UIState

--- src/slices/UISlice.ts
+++ src/slices/UISlice.ts
@@ -1,16 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Day, weekData } from "./workoutPlansSlice";
 
-const initialState = { logInProgress: false };
+interface NewWorkoutModalArgs {
+  remainingDays: Day[];
+  weekPosition: weekData["position"];
+  weekTitle: string;
+}
+
+interface UIState {
+  logInProgress: boolean;
+  newWorkoutModalData?: NewWorkoutModalArgs;
+}
+const initialState: UIState = { logInProgress: false };
 
 const slice = createSlice({
   name: "UI",
   initialState,
   reducers: {
-    setLogInProgress: (state, action: PayloadAction<boolean>) => {
+    setLogInProgress(state, action: PayloadAction<boolean>) {
       state.logInProgress = action.payload;
+    },
+    setNewWorkoutModalData(
+      state,
+      action: PayloadAction<NewWorkoutModalArgs | undefined>
+    ) {
+      state.newWorkoutModalData = action.payload;
     },
   },
 });
 
-export const { setLogInProgress } = slice.actions;
+export const { setLogInProgress, setNewWorkoutModalData } = slice.actions;
 export const UIReducer = slice.reducer;
