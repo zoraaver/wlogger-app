3cb7db932fb3a721660dfd7163672026312d836e
zoraaver <zoraaver@gmail.com>
2021-06-03 20:08:20 +0100

    Add empty list component and show activity indicator when data is being fetched

--- src/screens/WorkoutLogsScreen.tsx
+++ src/screens/WorkoutLogsScreen.tsx
@@ -1,6 +1,12 @@
 import { useFocusEffect } from "@react-navigation/core";
 import * as React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppDispatch, useAppSelector } from "..";
 import { HorizontalDivider } from "../components/HorizontalDivider";
@@ -9,6 +15,7 @@ import {
   getWorkoutLogs,
   workoutLogHeaderData,
 } from "../slices/workoutLogsSlice";
+import { Helvetica } from "../util/constants";
 
 export function WorkoutLogsScreen() {
   const dispatch = useAppDispatch();
@@ -23,23 +30,48 @@ export function WorkoutLogsScreen() {
     (state) => state.workoutLogs.data
   );
 
+  const dataPending = useAppSelector((state) => state.workoutLogs.dataPending);
+
   return (
     <SafeAreaView
       style={styles.workoutLogsScreen}
       edges={["bottom", "right", "left"]}
     >
-      <FlatList
-        style={{ flex: 1 }}
-        data={workoutLogs}
-        keyExtractor={(workoutLog: workoutLogHeaderData) => workoutLog._id}
-        renderItem={(data) => <WorkoutLogItem workoutLog={data.item} />}
-        ItemSeparatorComponent={() => <HorizontalDivider />}
-        ListFooterComponent={() => <HorizontalDivider />}
-      />
+      {dataPending && !workoutLogs.length ? (
+        <ActivityIndicator size="large" color="grey" />
+      ) : (
+        <FlatList
+          style={{ flex: 1 }}
+          data={workoutLogs}
+          keyExtractor={(workoutLog: workoutLogHeaderData) => workoutLog._id}
+          renderItem={(data) => <WorkoutLogItem workoutLog={data.item} />}
+          ItemSeparatorComponent={() => <HorizontalDivider />}
+          ListFooterComponent={() => <HorizontalDivider />}
+          ListEmptyComponent={() => (
+            <View style={styles.emptyListView}>
+              <Text style={styles.emptyListText}>No logs found</Text>
+            </View>
+          )}
+        />
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  workoutLogsScreen: { flex: 1, backgroundColor: "lightyellow" },
+  workoutLogsScreen: {
+    flex: 1,
+    backgroundColor: "lightyellow",
+    justifyContent: "center",
+  },
+  emptyListView: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "aliceblue",
+    height: 50,
+  },
+  emptyListText: {
+    fontSize: 20,
+    fontFamily: Helvetica,
+  },
 });
