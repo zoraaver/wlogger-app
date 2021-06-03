8a0d9a26b4bc66731779c4e27a36e5a2dac74973
zoraaver <zoraaver@gmail.com>
2021-06-03 12:12:17 +0100

    Add support for deep links from wlogger.uk

--- ios/wloggerApp/AppDelegate.m
+++ ios/wloggerApp/AppDelegate.m
@@ -7,39 +7,45 @@
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <React/RCTLinkingManager.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+  SKDescriptorMapper *layoutDescriptorMapper =
+      [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin:[[FlipperKitLayoutPlugin alloc]
+                            initWithRootNode:application
+                        withDescriptorMapper:layoutDescriptorMapper]];
   [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
   [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client addPlugin:[[FlipperKitNetworkPlugin alloc]
+                        initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
   [client start];
 }
 #endif
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self
+                                            launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"wloggerApp"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
-      rootView.backgroundColor = [UIColor systemBackgroundColor];
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
-      rootView.backgroundColor = [UIColor whiteColor];
+    rootView.backgroundColor = [UIColor whiteColor];
   }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -50,24 +56,47 @@ static void InitializeFlipper(UIApplication *application) {
   return YES;
 }
 - (void)applicationWillResignActive:(UIApplication *)application {
-    if (self.taskIdentifier != UIBackgroundTaskInvalid) {
-        [application endBackgroundTask:self.taskIdentifier];
-        self.taskIdentifier = UIBackgroundTaskInvalid;
-    }
-    
-    __weak typeof(self) weakSelf = self;
-    self.taskIdentifier = [application beginBackgroundTaskWithName:nil expirationHandler:^{
-        [application endBackgroundTask:weakSelf.taskIdentifier];
-        weakSelf.taskIdentifier = UIBackgroundTaskInvalid;
-    }];
+  if (self.taskIdentifier != UIBackgroundTaskInvalid) {
+    [application endBackgroundTask:self.taskIdentifier];
+    self.taskIdentifier = UIBackgroundTaskInvalid;
+  }
+
+  __weak typeof(self) weakSelf = self;
+  self.taskIdentifier = [application
+      beginBackgroundTaskWithName:nil
+                expirationHandler:^{
+                  [application endBackgroundTask:weakSelf.taskIdentifier];
+                  weakSelf.taskIdentifier = UIBackgroundTaskInvalid;
+                }];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:
+                (NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
+  return [RCTLinkingManager application:application
+                                openURL:url
+                                options:options];
+}
+
+- (BOOL)application:(UIApplication *)application
+    continueUserActivity:(nonnull NSUserActivity *)userActivity
+      restorationHandler:
+          (nonnull void (^)(NSArray<id<UIUserActivityRestoring>> *_Nullable))
+              restorationHandler {
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return
+      [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"
+                                                     fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"main"
+                                 withExtension:@"jsbundle"];
 #endif
 }
 
