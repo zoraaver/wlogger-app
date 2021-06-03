409c8d6d7568af079e599e7669ab362f09617b33
zoraaver <zoraaver@gmail.com>
2021-06-03 17:46:04 +0100

    Add error message when verification fails

--- src/screens/VerifyScreen.tsx
+++ src/screens/VerifyScreen.tsx
@@ -1,13 +1,14 @@
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/core";
 import * as React from "react";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import { useAppDispatch } from "..";
+import { useAppDispatch, useAppSelector } from "..";
+import { Button } from "../components/Button";
 import {
   UnauthenticatedNavigation,
   UnauthenticatedNavigatorStackParamList,
 } from "../navigators/UnauthenticatedStackNavigator";
 import { verifyUser } from "../slices/usersSlice";
-import { BalsamiqSans, infoColor } from "../util/constants";
+import { BalsamiqSans, Helvetica, infoColor } from "../util/constants";
 
 export function VerifyScreen() {
   const {
@@ -15,16 +16,13 @@ export function VerifyScreen() {
   } = useRoute<RouteProp<UnauthenticatedNavigatorStackParamList, "verify">>();
   const navigation = useNavigation<UnauthenticatedNavigation>();
   const dispatch = useAppDispatch();
+  const verificationError = useAppSelector(
+    (state) => state.user.verificationError
+  );
 
   React.useEffect(() => {
     if (verificationToken) {
-      dispatch(verifyUser(verificationToken))
-        .then((value) => {
-          if (value.type.includes("rejected")) {
-            navigation.navigate("login");
-          }
-        })
-        .catch(() => navigation.navigate("login"));
+      dispatch(verifyUser(verificationToken));
     } else {
       navigation.navigate("login");
     }
@@ -32,8 +30,21 @@ export function VerifyScreen() {
 
   return (
     <View style={styles.verifyScreen}>
-      <Text style={styles.verifyText}>Verifying email address</Text>
-      <ActivityIndicator size="large" color="aliceblue" />
+      {verificationError ? (
+        <>
+          <Text style={styles.verifyText}>
+            Oops something went wrong...{"\n"} Verification failed
+          </Text>
+          <Button onPress={() => navigation.navigate("login")}>
+            <Text style={styles.buttonText}>Back to login</Text>
+          </Button>
+        </>
+      ) : (
+        <>
+          <Text style={styles.verifyText}>Verifying email address</Text>
+          <ActivityIndicator size="large" color="aliceblue" />
+        </>
+      )}
     </View>
   );
 }
@@ -42,8 +53,18 @@ const styles = StyleSheet.create({
   verifyScreen: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
     backgroundColor: infoColor,
+    paddingHorizontal: 20,
   },
-  verifyText: { fontFamily: BalsamiqSans, fontSize: 22, paddingVertical: 20 },
+  verifyText: {
+    fontFamily: BalsamiqSans,
+    fontSize: 22,
+    paddingVertical: 20,
+    textAlign: "center",
+  },
+  buttonText: {
+    fontFamily: Helvetica,
+    fontSize: 18,
+    color: "white",
+  },
 });
