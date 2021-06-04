a6e3c290582e41e043511695a8ac1bb76c7cc6fc
zoraaver <zoraaver@gmail.com>
2021-06-04 14:01:06 +0100

    Display no internet alert when no internet available

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,21 @@ import { validateUser } from "../slices/usersSlice";
 import { HomeTabNavigator } from "../navigators/HomeTabNavigator";
 import { cleanCacheDirectory } from "../slices/workoutLogsSlice";
 import { UnauthenticatedStackNavigator } from "../navigators/UnauthenticatedStackNavigator";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { NoInternetAlert } from "../util/util";
 
 export function App() {
   const dispatch = useAppDispatch();
+
   const authenticationStatus = useAppSelector(
     (state) => state.user.authenticationStatus
   );
+
+  const { isInternetReachable } = useNetInfo();
+
+  React.useEffect(() => {
+    if (isInternetReachable === false) NoInternetAlert();
+  }, [isInternetReachable]);
 
   React.useEffect(() => {
     dispatch(validateUser());
