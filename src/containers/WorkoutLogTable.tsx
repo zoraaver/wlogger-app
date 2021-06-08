18b01234397f18dc27bfae20a027cfbbd06b3e2c
zoraaver <zoraaver@gmail.com>
2021-06-08 15:55:20 +0100

    Add onRowPress callback prop which passes exerciseIndex and setIndex as arguments

--- src/containers/WorkoutLogTable.tsx
+++ src/containers/WorkoutLogTable.tsx
@@ -10,13 +10,21 @@ import { DeviceOrientation, useOrientation } from "../util/hooks";
 import { renderRestInterval } from "../util/util";
 import { Table, MergeCell } from "./Table";
 
-type tableSetLogData = setLogData & { name?: exerciseLogData["name"] };
+type tableSetLogData = setLogData & {
+  name?: exerciseLogData["name"];
+  setIndex: number;
+  exerciseIndex: number;
+};
 
 interface WorkoutLogTableProps {
   workoutLog: workoutLogData;
+  onRowPress?: (exerciseIndex: number, setIndex: number) => void;
 }
 
-export function WorkoutLogTable({ workoutLog }: WorkoutLogTableProps) {
+export function WorkoutLogTable({
+  workoutLog,
+  onRowPress,
+}: WorkoutLogTableProps) {
   const orientation: DeviceOrientation = useOrientation();
   const tableHeaders = [
     "Exercise",
@@ -46,6 +54,13 @@ export function WorkoutLogTable({ workoutLog }: WorkoutLogTableProps) {
           renderRestInterval(setData.restInterval),
         ].map((text) => <Text style={styles.cellText}>{text}</Text>)
       }
+      onRowPress={
+        onRowPress
+          ? (rowData: tableSetLogData) =>
+              onRowPress(rowData.exerciseIndex, rowData.setIndex)
+          : undefined
+      }
+      rowKeyExtractor={(rowData) => (rowData._id || rowData.tempId) as string}
     />
   );
 }
@@ -53,10 +68,15 @@ export function WorkoutLogTable({ workoutLog }: WorkoutLogTableProps) {
 function mapExerciseLogDataToSetData(
   data: exerciseLogData[]
 ): Array<tableSetLogData> {
-  const sets: tableSetLogData[][] = data.map((logData: exerciseLogData) =>
-    logData.sets.length > 0
-      ? [{ ...logData.sets[0], name: logData.name }, ...logData.sets.slice(1)]
-      : []
+  const sets: tableSetLogData[][] = data.map(
+    (logData: exerciseLogData, exerciseIndex: number) =>
+      logData.sets.map((set: setLogData, setIndex: number) => {
+        if (setIndex === 0) {
+          return { ...set, setIndex, exerciseIndex, name: logData.name };
+        } else {
+          return { ...set, setIndex, exerciseIndex };
+        }
+      })
   );
   return sets.flat();
 }
