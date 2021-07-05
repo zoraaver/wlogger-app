3c4b8668460234c461a6fd0744aaec3f361f3d65
zoraaver <zoraaver@gmail.com>
2021-07-05 18:57:30 +0100

    Import url's from keys.json

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import { Platform } from "react-native";
 import { getToken } from "../util/util";
+import {
+  BACKEND_PRODUCTION_URL,
+  IOS_DEV_URL,
+  ANDROID_DEV_URL,
+} from "./keys.json";
 
-export const backendUrl: string = "https://wlogger.uk/api";
+export const backendUrl: string = BACKEND_PRODUCTION_URL;
 const developmentUrl: string =
-  Platform.OS === "ios" ? "http://192.168.86.25:8080" : "http://10.0.2.2:8080";
+  Platform.OS === "ios" ? IOS_DEV_URL : ANDROID_DEV_URL;
 
 export const baseURL =
   process.env.NODE_ENV === "production" ? backendUrl : developmentUrl;
